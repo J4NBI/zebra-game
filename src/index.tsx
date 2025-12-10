a4ac9730f4974fa634474ab08c21e5ae63652f90
Jan Bihl <janbihl@MacBook-Pro-von-Jan.local>
2025-12-10 23:16:45 +0100

    dploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
