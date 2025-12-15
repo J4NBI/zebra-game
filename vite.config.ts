ff78e387b4c5d15b0c9300cbfb6ef429cdfa39d6
J4N <142495328+J4NBI@users.noreply.github.com>
2025-12-15 10:18:38 +0000

    v6

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/zebra-game/", // <--- hier deinen Repo-Namen einsetzen
+  base: "/", // <--- hier deinen Repo-Namen einsetzen
 });
