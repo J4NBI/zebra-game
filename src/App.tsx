9e173bddfec995c70e9fa9eb1e08a4c96bdabb6a
J4N <142495328+J4NBI@users.noreply.github.com>
2025-12-15 09:01:38 +0000

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CreateZebra from "./components/CreateZebra.tsx";
 export default function App() {
   return (
     <div className="App">
-        {}<h1>ZEBRA ICE</h1>
+        <h1>ZEBRA ICE</h1>
 
       <div className="game">
         <CreateZebra />
